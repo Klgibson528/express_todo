f25098213c764163ce454fa23a29aa1a1416de25
Katy Gibson <klgibson528@gmail.com>
2018-07-13 21:36:03 -0500

    login displays todos

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const body_parser = require("body-parser");
 const app = express();
 const pgp = require("pg-promise")({});
-var bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const expressValidator = require("express-validator");
 const saltRounds = 10;
 const db = pgp(
   process.env.DATABASE_URL || { database: "todo", user: "postgres" }
@@ -22,14 +23,22 @@ app.use(
     extended: false
   })
 );
+app.use(expressValidator());
 
-app.get("/signin", function(req, resp) {
-  
-  db.query("SELECT * FROM task WHERE userid = ($1)", userid).then(function(
+app.post("/", function(req, resp) {
+  var email = req.body.InputEmail1;
+
+  db.query("SELECT userid FROM users WHERE email = ($1)", email).then(function(
     results
   ) {
-    resp.render("todolist.html", {
-      results: results
+    userid = results[0].userid;
+    console.log(userid);
+    db.query("SELECT * FROM task WHERE userid = ($1)", userid).then(function(
+      results
+    ) {
+      resp.render("todolist.html", {
+        results: results
+      });
     });
   });
 });
@@ -42,15 +51,14 @@ app.get("/", function(req, res, next) {
   res.render("signin.html");
 });
 
-
-app.post("/", function(req, resp, next) {
+app.post("/update", function(req, resp, next) {
   var id = req.body.id;
   db.query(
     "UPDATE task SET done = true WHERE id = ($1) userid = ($2)",
     id,
     userid
   );
-  resp.redirect("/");
+  resp.render("todolist.html");
 });
 
 app.post("/add", function(req, resp, next) {
@@ -69,6 +77,41 @@ app.post("/new_user", function(req, resp, next) {
   var hash = bcrypt.hashSync(password, saltRounds);
   var fname = req.body.inputFName;
   var lname = req.body.inputLName;
+
+  req
+    .checkBody("email", "The email you entered is invalid, please try again.")
+    .isEmail();
+  req
+    .checkBody(
+      "email",
+      "Email address must be between 4-100 characters long, please try again."
+    )
+    .len(4, 100);
+  req
+    .checkBody(
+      "inputPassword",
+      "Password must be between 8-100 characters long."
+    )
+    .len(8, 100);
+  req
+    .checkBody(
+      "inputPassword",
+      "Password must include one lowercase character, one uppercase character, a number, and a special character."
+    )
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/,
+      "i"
+    );
+  req
+    .checkBody(
+      "inputPassword2",
+      "Password must be between 8-100 characters long."
+    )
+    .len(8, 100);
+  req
+    .checkBody("inputPassword2", "Passwords do not match, please try again.")
+    .equals(req.body.inputPassword);
+
   console.log(email, hash, lname, fname);
   db.query(
     "INSERT INTO users (userid, email, password, last_name, first_name) VALUES (default, $1, $2, $3, $4)",
